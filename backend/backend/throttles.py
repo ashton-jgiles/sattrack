f0033e441bf0e8d868940932cfa914f6160aa608
ashtonjgiles <ashtonjgiles@gmail.com>
2026-03-29 19:01:45 -0600

    feat: add documentation to backend directory

--- backend/backend/throttles.py
+++ backend/backend/throttles.py
@@ -1,5 +1,7 @@
+# api throttling imports
 from rest_framework.throttling import UserRateThrottle
 
+# create classes for our most expensive apis being the retrive from celestrak and retrive all positional data for the globe
 class CelesTrakThrottle(UserRateThrottle):
     scope = 'celestrak'
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# imports for the app urls to route to the correct urls.py file
 from django.contrib import admin
 from django.urls import path, include
 
+# urls for routes
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/satellite/', include('satellites.urls')),
     path('api/dataset/', include('datasets.urls')),
     path('api/manage/satellite/', include('manage_satellites.urls')),
