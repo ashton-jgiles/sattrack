00eb223e396e0ae3043594e84199dfe2879cdb5e
Ashton Giles <ashtonjgiles@gmail.com>
2026-04-06 20:42:13 -0600

    [143] Settings Page (#10)
    * feat: add endpoints for user settings
    
    * feat: create frontend components for settings and viewing user profile
    
    * feat: connect settings page to dashboard
    
    * feat: add backend endpoints to users app
    
    * chore: remove unused styles
    
    * feat: update style names
    
    * feat: add sql endpoints to users views
    
    * feat: connect update trajectory background function to frontend
    
    * chore: cleanup minor bugs

--- backend/frontend/src/api/userService.jsx
+++ backend/frontend/src/api/userService.jsx
@@ -1,9 +1,31 @@
 // import post for registering new users
-import { post } from "./api";
+import { get, post, del } from "./api";
+
+// get all users
+export const getUsers = async () => {
+  const data = await get(`/auth/users/`);
+  return data;
+};
+
+// get specific user
+export const getUserProfile = async (username) => {
+  const data = await get(`/auth/${username}/userProfile/`);
+  return data;
+};
+
+// modify a user
+export const modifyUser = async (payload) => {
+  return await post(`/auth/user/modify/`, payload);
+};
+
+// delete a user
+export const deleteUser = async (username) => {
+  return await del(`/auth/user/${username}/delete/`);
+};
 
 // register user method to create a new user in the database
 export const registerUser = async (username, fullName, password) => {
-  return await post("/auth/register/", {
+  return await post(`/auth/register/`, {
     username,
     full_name: fullName,
     password,
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('register/', views.CreateAccountView.as_view()),
+    path('users/', views.GetUsers.as_view()),
+    path('<str:username>/userProfile/', views.GetUserProfile.as_view()),
+    path('user/modify/', views.ModifyUser.as_view()),
+    path('user/<str:username>/delete/', views.DeleteUser.as_view()),
 ]
