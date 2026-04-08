f972469c7e1e722df5eab63ddb9139352ef70933
ashtonjgiles <ashtonjgiles@gmail.com>
2026-04-08 11:24:11 -0600

    chore: document and clean up settings app

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-# lookups app urls
+# settings app urls
 urlpatterns = [
     path('trajectory/update/', views.UpdateTrajectories.as_view()),
 ]
--- backend/settings/views.py
+++ backend/settings/views.py
@@ -1,16 +1,21 @@
-# rest api imports
+# rest api imports, and command and threading imports for updating trajectory asynchronously
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.core.management import call_command
+import threading
 
 # update trajectory endpoint to create background thread to update all trajectory
 class UpdateTrajectories(APIView):
     def post(self, request):
-        from django.core.management import call_command
-        import threading
+        # create the thread
         thread = threading.Thread(
+            # call the command
             target=call_command,
             args=('update_trajectories',),
             daemon=True
         )
+        # start the start
         thread.start()
+        # return the success response
         return Response({'message': 'Trajectory update started'})
+    
